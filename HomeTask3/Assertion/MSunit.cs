5a946352fd5dc77ae14205b9bbfb465f40fd86d4
Shahad Mohamed Nazeer <shahadnazeer22@gmail.com>
2022-10-31 14:33:12 +0530

    Add project files.

---
+++ HomeTask3/Assertion/MSunit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpCalculator;
+
+namespace HomeTask3.Assertion
+{
+
+    [TestClass]
+    public class MSunit
+    {
+        public TestContext TestContext { get; set; }
+        [ClassInitialize]
+        public static void ClassInit(TestContext x)
+        {
+            Console.WriteLine("Preconditions for test class");
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            Console.WriteLine("Preconditions for test");
+        }
+
+        [DataTestMethod]
+        [DataRow(8, 4, 4)]
+        [DataRow(10, 5, 5)]
+        [DataRow(10, 10, 0)]
+        public void MSunitTest(double x, double y, double e)
+        {
+            Calculator cal = new Calculator();
+            Assert.AreEqual(cal.Sub(x, y), e);
+        }
+
+        [DataTestMethod]
+        [DataRow("2", "1", "1")]
+        public void MSunitTest1(string x, string y, string e)
+        {
+            double xnew = Convert.ToDouble(x);
+            double ynew = Convert.ToDouble(y);
+            double enew = Convert.ToDouble(e);
+            Calculator cal = new Calculator();
+            Assert.AreEqual(cal.Sub(xnew, ynew), enew);
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            Console.WriteLine("Post conditions for test");
+        }
+
+        [ClassCleanup]
+        public static void CleanUp()
+        {
+            Console.WriteLine("Post conditions for test class");
+        }
+    }
+}
---
+++ HomeTask3/Assertion/Nunit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using CSharpCalculator;
+
+[assembly: LevelOfParallelism(2)]
+namespace HomeTask3.Assertion
+{
+    [Parallelizable(ParallelScope.All)]
+    [TestFixture]
+    public class NUnit
+    {
+        [OneTimeSetUp]
+        public static void ClassInit()
+        {
+            Console.WriteLine("Preconditions for test class");
+        }
+
+        [SetUp]
+        public static void TestInit()
+        {
+            Console.WriteLine("Preconditions for test");
+        }
+
+        [TestCase(4,8,12)]
+        [TestCase(5,10,15)]
+        [TestCase(10, 10, 20)]
+        public void NunitTest(double x, double y, double e)
+        { 
+             Calculator cal= new Calculator();
+             Assert.AreEqual(cal.Add(x, y), e);
+        }
+
+        [TestCase("1", "1", "2")]
+        public void NunitTest1(string x, string y, string e)
+        {
+            double xnew = Convert.ToDouble(x);
+            double ynew = Convert.ToDouble(y);
+            double enew = Convert.ToDouble(e);
+            Calculator cal = new Calculator();
+            Assert.AreEqual(cal.Add(xnew, ynew), enew);
+        }
+
+        [TearDown]
+        public static void TestClean()
+        {
+            Console.WriteLine("Post conditions for test");
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            Console.WriteLine("Post conditions for test class");
+        }
+    }
+}
